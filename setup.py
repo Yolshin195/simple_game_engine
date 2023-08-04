c9ff98aada74a7a939580a5c5239689186f77974
Yolshin <elshin195@gmail.com>
2023-08-04 16:53:44 +0700

    test import game engine

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='yg_simple_game_engine',
+    name='yg-simple-game-engine',
     version='0.1',
     package_dir={'': 'src'},
     packages=find_packages('src'),
